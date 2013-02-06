0ffcf4a265faa32c8953555201e7ba7a85593bc0
Marco Martinez <usblackbird@gmail.com>
2013-02-05 23:52:11 -0800

    Added Assets/Settings

---
+++ ch06-mrnom/src/com/badlogic/androidgames/mrnom/Settings.java
@@ -0,0 +1,16 @@
+package com.badlogic.androidgames.mrnom;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import com.badlogic.androidgames.framework.FileIO;
+
+public class Settings {
+  public static boolean soundEnabled = true;
+  public static int[] highscores = new int[] {100, 80, 50, 30, 10};
+
+
+}
