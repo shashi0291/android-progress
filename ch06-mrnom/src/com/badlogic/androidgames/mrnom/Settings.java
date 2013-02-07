49024c190453362cb7a671f87683160ae6ef076f
Marco Martinez <usblackbird@gmail.com>
2013-02-06 23:44:59 -0800

    Completed settings

--- ch06-mrnom/src/com/badlogic/androidgames/mrnom/Settings.java
+++ ch06-mrnom/src/com/badlogic/androidgames/mrnom/Settings.java
@@ -12,5 +12,60 @@ public class Settings {
   public static boolean soundEnabled = true;
   public static int[] highscores = new int[] {100, 80, 50, 30, 10};
 
+  public static void load(FileIO files) {
+    BufferedReader in = null;
+    try {
+      // The book mentions that it is probably best to use the API 8 method
+      // Context.getExternalStorage() for loading app-specific files
+      in = new BufferedReader(new InputStreamReader(files.readFile(".mrnom")));
+      soundEnabled = Boolean.parseBoolean(in.readLine());
+      for (int i = 0; i < 5; i++) {
+        highscores[i] = Integer.parseInt(in.readLine());
+      }
+    } catch (IOException e) {
+      // :( It's ok we have defaults
+    } catch (NumberFormatException e) {
+      // :/ It's ok, defaults save our day
+    } finally {
+      try {
+        if (in != null) {
+          in.close();
+        }
+      } catch (IOException e) {
+        // Well screw it, we tried
+      }
+    }
+  }
 
+  public static void save(FileIO files) {
+    BufferedWriter out = null;
+    try {
+      out = new BufferedWriter(new OutputStreamWriter(files.writeFile(".mrnom")));
+      out.write(Boolean.toString(soundEnabled));
+      for (int i = 0; i < 5; i++) {
+        out.write(Integer.toString(highscores[i]));
+      }
+    } catch (IOException e){
+      // Should probably inform the user that something went wrong while saving
+    } finally {
+      try {
+        if (out != null) {
+          out.close();
+        }
+      } catch (IOException e) {
+      }
+    }
+  }
+
+  public static void addScores(int score) {
+    for (int i = 0; i < 5; i++) {
+      if (highscores[i] < score) {
+        for (int j = 4; j > i; j--){
+          highscores[j] = highscores[j - 1];
+        }
+        highscores[i] = score;
+        break;
+      }
+    }
+  }
 }
